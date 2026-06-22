acc8dcfd3b70f58b00201736323ecec5a558ae0c
anshucodes404 <anshukr95086@gmail.com>
2026-06-22 12:04:43 +0530

    working now

--- pass_manager/src/main.rs
+++ pass_manager/src/main.rs
@@ -1,9 +1,9 @@
 use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::fs::{self, File};
-use std::io::BufWriter;
+use std::io::{self, BufWriter, Write};
 
-#[derive(Debug, Deserialize, Serialize, PartialEq)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
 #[serde(rename_all = "camelCase")]
 struct Credential {
     website_name: String,
@@ -23,6 +23,7 @@ fn menu() {
     println!("q. Exit");
     println!("============================");
     print!("Enter your choice: ");
+    io::stdout().flush().expect("failed to flush the output");
 }
 
 fn load_credentials() -> Vec<Credential> {
@@ -30,6 +31,69 @@ fn load_credentials() -> Vec<Credential> {
         fs::read_to_string(PATH).expect("Not able to read credentials.json file");
 
     serde_json::from_str(&prev_credentials).expect("Failed to parse json")
+}
+
+fn add_credential(credentials: &mut Vec<Credential>) {
+    println!("You chose to add a credential. \n Fill the required details");
+
+    let website_name = take_input("Website Name");
+    let user_name = take_input("User Name");
+    let password = take_input("Password");
+
+    let credential = Credential {
+        website_name,
+        user_name,
+        password,
+    };
+
+    if credentials.contains(&credential) {
+        println!("Credential already exists");
+    } else {
+        credentials.push(credential);
+        println!("Credential addedd successfully");
+    }
+}
+
+fn search_on_website_name(credentials: &[Credential]) {
+    let website_name = take_input("Website Name");
+
+    print_all_credentials(
+        credentials
+            .iter()
+            .filter(move |cred| cred.website_name == website_name),
+    );
+}
+
+fn search_on_user_name(credentials: &[Credential]) {
+    let user_name = take_input("User Name");
+
+    print_all_credentials(
+        credentials
+            .iter()
+            .filter(move |cred| cred.user_name == user_name),
+    );
+}
+
+fn search_on_website_name_and_user_name(credentials: &[Credential]) {
+    let website_name = take_input("Website Name");
+    let user_name = take_input("User Name");
+
+    print_all_credentials(
+        credentials
+            .iter()
+            .filter(move |cred| cred.user_name == user_name && cred.website_name == website_name),
+    );
+}
+
+fn delete_credential(credentials: &mut Vec<Credential>) {
+    println!("You chose to delete a credential. \nFill the required details");
+
+    let website_name = take_input("Website Name");
+    let user_name = take_input("User Name");
+
+    credentials.retain(|credential| {
+        credential.user_name != user_name || credential.website_name != website_name
+    });
 }
 
 fn save_file_before_exit(credentials: &[Credential]) -> Result<(), Box<dyn Error>> {
@@ -41,47 +105,142 @@ fn save_file_before_exit(credentials: &[Credential]) -> Result<(), Box<dyn Error
     Ok(())
 }
 
-fn add_credential(credential: Credential, credentials: &mut Vec<Credential>) {
-    if credentials.contains(&credential) {
-        println!("Credential already exists");
-    } else {
-        credentials.push(credential);
-        println!("Credential addedd successfully");
+fn take_input(prompt: &str) -> String {
+    loop {
+        print!("{}: ", prompt);
+
+        let mut input = String::new();
+
+        io::stdout().flush().expect("Failed to flush the output");
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read the input");
+
+        let input = input.trim();
+
+        if input.len() == 0 {
+            println!("{} cannot be empty. Please try again. \n", prompt);
+            continue;
+        }
+
+        return input.to_string();
     }
 }
 
-fn search_on_website_name<'a>(credentials: &'a [Credential], website_name: &str) -> Vec<&'a Credential> {
-    credentials
-        .iter()
-        .filter(|cred| cred.website_name == website_name)
-        .collect()
+fn menu_for_search() {
+    println!("========== SEARCH MENU ==========");
+    println!("1. Search from Website Name");
+    println!("2. Search from User Name");
+    println!("3. Search from Website and User Name");
 }
 
-fn search_on_user_name<'a>(credentials: &'a [Credential], user_name: &str) -> Vec<&'a Credential> {
-    credentials
-        .iter()
-        .filter(|cred| cred.user_name == user_name)
-        .collect()
+fn choice_for_search() -> String {
+
+    println!("\nEnter your choice");
+
+    io::stdout().flush().expect("Failed to flush the output");
+    
+    let mut choice = String::new();
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read the choice");
+    choice
 }
 
-fn search_on_website_name_and_user_name<'a>(credentials: &'a [Credential], user_name: &str, website_name: &str) -> Vec<&'a Credential> {
-    credentials
-        .iter()
-        .filter(|cred| cred.user_name == user_name && cred.website_name == website_name)
-        .collect()
+fn print_all_credentials<'a, I>(credentials: I)
+where
+    I: IntoIterator<Item = &'a Credential>,
+{
+    println!("==================== YOUR CREDENTIALS ====================");
+    println!(
+        "{:<35}{:<20}{:<20}",
+        "WEBSITE NAME", "USER NAME", "PASSWORD"
+    );
+    for cred in credentials {
+        println!(
+            "{:<35}{:<20}{:<20}",
+            cred.website_name, cred.user_name, cred.password
+        );
+    }
 }
 
-fn delete_credential(credentials: &mut Vec<Credential>, user_name: &str, website_name: &str) {
-    credentials
-        .retain(|credential| credential.user_name != user_name || credential.website_name != website_name);
+fn match_choice_for_search(choice: &str, credentials: &[Credential]) {
+    match choice {
+        "1" => {
+            search_on_website_name(credentials);
+        }
+
+        "2" => {
+            search_on_user_name(credentials);
+        }
+
+        "3" => {
+            search_on_website_name_and_user_name(credentials);
+        }
+
+        _ => {
+            println!("INVALID SEARCH CHOICE");
+        }
+    }
 }
 
-fn main() {
-    let credentials: Vec<Credential> = load_credentials();
+fn match_the_choice(
+    choice: &str,
+    credentials: &mut Vec<Credential>,
+) -> Result<bool, Box<dyn Error>> {
+    match choice {
+        "1" => {
+            add_credential(credentials);
+            Ok(false)
+        }
+
+        "2" => {
+            menu_for_search();
+            let search_choice = choice_for_search();
+            match_choice_for_search(&search_choice, credentials);
+            Ok(false)
+        }
+
+        "3" => {
+            print_all_credentials(credentials.iter());
+            Ok(false)
+        }
+
+        "4" => {
+            delete_credential(credentials);
+            Ok(false)
+        }
+
+        "q" => {
+            save_file_before_exit(credentials)?;
+            Ok(true)
+        }
+
+        _ => {
+            println!("INVALID CHOICE");
+            Ok(false)
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut credentials: Vec<Credential> = load_credentials();
 
     // println!("{:?}", credentials)
 
     loop {
         menu();
+
+        let mut choice = String::new();
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read choice");
+
+        if match_the_choice(choice.trim(), &mut credentials)? {
+            break;
+        }
     }
+
+    Ok(())
 }
